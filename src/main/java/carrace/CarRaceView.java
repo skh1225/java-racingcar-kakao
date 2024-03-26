b285a7c08d09a0dc56b9592100a50882edd8a2dd
chris.tmars <chris.tmars@kakaocorp.com>
2024-03-26 14:34:39 +0900

    feat: CarRaceView에서 입력에 대한 유효성 검사

--- src/main/java/carrace/CarRaceView.java
+++ src/main/java/carrace/CarRaceView.java
@@ -2,8 +2,10 @@ package carrace;
 
 import carrace.domain.Car;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,13 +15,42 @@ public class CarRaceView {
 
 	public List<String> getCarNames() {
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-		return Stream.of(scanner.nextLine().split(","))
-			.collect(Collectors.toList());
+		List<String> carNames = Stream.of(scanner.nextLine().split(",")).collect(Collectors.toList());
+		for (String carName : carNames) {
+			validateName(carName);
+		}
+		validateDuplicateNames(carNames);
+		return carNames;
+	}
+
+	public void validateName(String carName) {
+		if (carName.length() > 5) {
+			throw new IllegalArgumentException("자동차의 이름은 5자이하여야 합니다.");
+		}
+
+		if (carName.isEmpty()) {
+			throw new IllegalArgumentException("자동차의 이름은 공백이 아니여야 합니다.");
+		}
+	}
+
+	public void validateDuplicateNames(List<String> carNames) {
+		Set<String> carNamesSet = new HashSet<>(carNames);
+		if (carNames.size() != carNamesSet.size()) {
+			throw new IllegalArgumentException("중복된 이름이 존재합니다.");
+		}
 	}
 
 	public int getCarRaceRound() {
 		System.out.println("시도할 횟수는 몇회인가요?");
-		return Integer.parseInt(scanner.nextLine());
+		int round = Integer.parseInt(scanner.nextLine());
+		validateRaceRound(round);
+		return round;
+	}
+
+	public void validateRaceRound(int round) {
+		if (round <= 0) {
+			throw new IllegalArgumentException("라운드는 양수여야 합니다.");
+		}
 	}
 
 	public void displayResultStartMessage() {
@@ -30,7 +61,7 @@ public class CarRaceView {
 		cars.stream()
 			.map(car -> car.getName() + " : " + "-".repeat(car.getPosition()))
 			.forEach(System.out::println);
-		System.out.println("");
+		System.out.println();
 	}
 
 	public void displayWinnerNames(List<String> winnerNames) {
--- src/main/java/carrace/domain/Car.java
+++ src/main/java/carrace/domain/Car.java
@@ -2,20 +2,11 @@ package carrace.domain;
 
 public class Car {
 
-    private static final int MAX_NAME_LENGTH = 5;
-
     private final String name;
     private int position = 1;
 
     public Car(String name) {
-        validateName(name);
         this.name = name;
-    }
-
-    private void validateName(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException(String.format("이름은 %d글자 이하여야합니다.", MAX_NAME_LENGTH));
-        }
     }
 
     public String getName() {
