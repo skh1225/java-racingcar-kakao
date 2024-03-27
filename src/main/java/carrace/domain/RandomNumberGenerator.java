c11c8ae001c4ab243d47a6f5dcf6501a7d1d036a
chris.tmars <chris.tmars@kakaocorp.com>
2024-03-27 17:10:51 +0900

    refactor: Random 객체를 static 으로 변경

--- src/main/java/carrace/domain/RandomNumberGenerator.java
+++ src/main/java/carrace/domain/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
-    private final Random random = new Random();
+    private static final Random random = new Random();
     private static final int MAX_RANDOM_NUMBER = 9;
     private static final int MIN_RANDOM_NUMBER = 0;
     @Override
