590fc35d907a2cfcf4ef0781f0a5038e6e231dc5
chris.tmars <chris.tmars@kakaocorp.com>
2024-03-27 17:41:23 +0900

    refactor: CarMoveRuleTest에 DisplayName을 추가

--- src/test/java/carrace/CarMoveRuleTest.java
+++ src/test/java/carrace/CarMoveRuleTest.java
@@ -1,6 +1,8 @@
 package carrace;
 
 import carrace.domain.CarMoveRule;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,6 +12,7 @@ public class CarMoveRuleTest {
 
     @CsvSource({"0, false", "3, false", "4, true", "9, true"})
     @ParameterizedTest
+    @DisplayName("자동차는 생성된 숫자가 4이상이면 움직입니다.")
     void move(int value, boolean move) {
         CarMoveRule carMoveRule = new CarMoveRule(() -> value);
         boolean result = carMoveRule.isMovable();
