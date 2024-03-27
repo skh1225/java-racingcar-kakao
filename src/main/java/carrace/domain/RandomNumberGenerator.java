28d09a629572e425ecac07cc8876c2feac99c311
chris.tmars <chris.tmars@kakaocorp.com>
2024-03-27 16:54:44 +0900

    refactor: Random 랜덤 객체 생성 비용 절감

--- src/main/java/carrace/domain/RandomNumberGenerator.java
+++ src/main/java/carrace/domain/RandomNumberGenerator.java
@@ -4,8 +4,11 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
+    private final Random random = new Random();
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int MIN_RANDOM_NUMBER = 0;
     @Override
     public int generate() {
-        return (new Random()).nextInt(10);
+        return random.nextInt(MAX_RANDOM_NUMBER-MIN_RANDOM_NUMBER+1) + MIN_RANDOM_NUMBER;
     }
 }
